41ab75a8415eca9e6ea7c6d61298dfd0fb95dabf
SangHyun <duddjshfl@naver.com>
2023-11-13 21:38:16 +0900

    chore: 주석 추가

--- src/sample/sample1.jsx
+++ src/sample/sample1.jsx
@@ -7,6 +7,8 @@ const Sample1 = () => {
 
   return (
     <div className='sample--first'>
+      {/* 각각 개별로 ErrorBoundary가 적용이 된다. */}
+      {/* ErrorBoundary는 현재 child에 적용된 컴포넌트에 적용되고, 가장 가까운 ErrorBoundary를 만나게 되면 상위로 더 이상 에러가 전달되지 않는다 */}
       <div>
         <h3>first counter</h3>
         <ErrorBoundary>
