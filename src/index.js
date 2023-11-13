f0c08e4a3a4ad9a45d00952dda82219edaef4579
SangHyun <duddjshfl@naver.com>
2023-11-13 20:43:13 +0900

    feat: ErrorBoundary 컴포넌트 적용

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
