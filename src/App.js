047feabbf047880360336193588be05deb82f21c
SangHyun <duddjshfl@naver.com>
2023-11-13 20:42:54 +0900

    feat: 카운터 컴포넌트 렌더링에 포함

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Counter from './components/Counter'
 import './App.css'
 
 function App() {
   return (
     <div>
-      Test ErrorBoundary
+      <h2>Test ErrorBoundary</h2>
+      <Counter />
     </div>
   )
 }
